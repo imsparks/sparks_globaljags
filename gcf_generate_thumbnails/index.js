27e509b62e0acca839de3d757c07f0c4cd95284a
Isaiah Sparks <imsparks@iu.edu>
2024-03-03 21:38:53 +0000

    End of full gcf - no versions necessary

--- gcf_generate_thumbnails/index.js
+++ gcf_generate_thumbnails/index.js
@@ -5,8 +5,13 @@ const fs = require('fs-extra');
 const os = require('os');
 const sharp = require('sharp');
 
+const getExif = require('exif-async');
+const parseDMS = require('parse-dms');
+
+const {Firestore} = require('@google-cloud/firestore');
+
 // Entry point function
-exports.generateThumbnail = async (file, context) => {
+exports.generate_thumb_data = async (file, context) => {
   const gcsFile = file;
   const storage = new Storage();
   const sourceBucket = storage.bucket(gcsFile.bucket);
@@ -59,6 +64,19 @@ exports.generateThumbnail = async (file, context) => {
       destination: tempFilePath
     });
 
+
+
+    //Here is where I will extract exif data to enter into FireStore
+    let gpsObject = await readExifData(tempFilePath);
+    console.log(gpsObject);
+    let gpsDecimal = getGPSCoordinates(gpsObject);
+    console.log(gpsDecimal);
+    console.log(gpsDecimal.lat);
+    console.log(gpsDecimal.lon);
+
+
+
+
     // Upload our local version of the file to the final images bucket
     await finalBucket.upload(tempFilePath);
     
@@ -79,10 +97,57 @@ exports.generateThumbnail = async (file, context) => {
     // Delete the temp working directory and its files from the GCF's VM
     await fs.remove(workingDir);
     
+
+      // Here is where I will connect to FireStore and create the objects
+    const firestore = new Firestore({
+      projectId: "sp24-41200-sparks-globaljags",
+      // databaseId: "name if you changed it from default"
+    });
+
+    const photoData = {
+      imageName: `${finalFileName}`,
+      imageUrl: thumbnailsBucket.file(finalFileName).publicUrl(),
+      lat:gpsDecimal.lat,
+      lon:gpsDecimal.lon,
+      thumbURL: finalBucket.file(thumbName).publicUrl()
+    };
+
+    let collectionRef = firestore.collection('photos');
+    let documentRef = await collectionRef.add(photoData);
+    console.log(`Document created: ${documentRef.id}`);
+  
   } // end of validFile==true
 
   // DELETE the original file uploaded to the "Uploads" bucket
   await sourceBucket.file(gcsFile.name).delete();
   console.log(`Deleted uploaded file: ${gcsFile.name}`);
+}
 
+
+
+// Helper Functions
+async function readExifData(localFile){
+  let exifData;
+  try{
+      exifData = await getExif(localFile);
+      //console.log(exifData);
+      // console.log(exifData.gps);
+      // console.log(exifData.gps.GPSLatitude);
+      return exifData.gps;
+
+  } catch(err){
+      console.log(err);
+      return null;
+  }
+}
+
+function getGPSCoordinates(g){
+  // PARSE DMS needs a string in format of:
+  // 51:30:0.5486N 0:7:34.4503W
+  // DEG:MIN:SECDIRECTION DEG:MIN:SECDIRECTION
+  const latString = `${g.GPSLatitude[0]}:${g.GPSLatitude[1]}:${g.GPSLatitude[2]}${g.GPSLatitudeRef}`;
+  const lonString = `${g.GPSLongitude[0]}:${g.GPSLongitude[1]}:${g.GPSLongitude[2]}${g.GPSLongitudeRef}`;
+
+  const degCoords = parseDMS(`${latString} ${lonString}`);
+  return degCoords;
 }
